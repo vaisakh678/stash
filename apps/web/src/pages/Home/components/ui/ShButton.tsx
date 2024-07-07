d5ed02fb4c40a215033cbfe6024a3e0ad5fd99d4
vaisakh b <vaisakh@vaisakhs-Air.domain.name>
2024-07-07 14:49:47 +0530

    NavLink refactoring.

--- apps/web/src/pages/Home/components/ui/ShButton.tsx
+++ apps/web/src/pages/Home/components/ui/ShButton.tsx
@@ -15,7 +15,7 @@ const ShButton: React.FC<ShButtonProps> = ({ children, StartIcon, to, onClick })
 		<NavLink to={to} onClick={onClick}>
 			{({ isActive }) => (
 				<div className={`h-10 w-full text-left flex items-center hover:bg-gray-100 relative ${isActive ? "bg-gray-200" : ""}`}>
-					{isActive ? <div className="bg-black w-1 h-full absolute" /> : null}
+					<div className={`w-1 h-full absolute transition-colors ${isActive ? "bg-black" : "bg-inherit"}`} />
 					<div className="mx-2 ml-3">
 						<StartIcon />
 					</div>
