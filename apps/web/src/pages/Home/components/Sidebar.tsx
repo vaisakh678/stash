fe315de925425471843ef96a95609291bf0d9cf5
vaisakh b <vaisakh@vaisakhs-Air.domain.name>
2024-07-07 14:30:44 +0530

    updated the sidebar navigation with NavLink.

--- apps/web/src/pages/Home/components/Sidebar.tsx
+++ apps/web/src/pages/Home/components/Sidebar.tsx
@@ -3,11 +3,8 @@ import ShButton from "./ui/ShButton";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ShowChartIcon from "@mui/icons-material/ShowChart";
 import TuneIcon from "@mui/icons-material/Tune";
-import { useLocation, useNavigate } from "react-router-dom";
 import { Drawer } from "@mui/material";
 import { useSidebar } from "../../../context/SidebarProvider";
-
-type Menu = "none" | "dashboard" | "create-loan" | "settings";
 
 const Sidebar: React.FC = () => {
 	const { isDrawerOpen, handleCloseDrawer } = useSidebar();
@@ -28,18 +25,6 @@ const Sidebar: React.FC = () => {
 };
 
 export const SidebarContent = ({ onClose }: { onClose?: () => void }) => {
-	const navigate = useNavigate();
-	const location = useLocation();
-
-	let menu: Menu = "none";
-	if (location.pathname.includes("dashboard")) {
-		menu = "dashboard";
-	} else if (location.pathname.includes("create-loan")) {
-		menu = "create-loan";
-	} else if (location.pathname.includes("settings")) {
-		menu = "settings";
-	}
-
 	return (
 		<>
 			<header className="h-[60px] flex items-center px-5">
@@ -47,38 +32,17 @@ export const SidebarContent = ({ onClose }: { onClose?: () => void }) => {
 			</header>
 			<ul className="flex flex-col gap-2 mt-14">
 				<li>
-					<ShButton
-						StartIcon={DashboardIcon}
-						active={menu === "dashboard"}
-						onClick={() => {
-							onClose && onClose();
-							navigate("/dashboard");
-						}}
-					>
+					<ShButton StartIcon={DashboardIcon} to="/dashboard" onClick={onClose}>
 						Dashboard
 					</ShButton>
 				</li>
 				<li>
-					<ShButton
-						StartIcon={ShowChartIcon}
-						active={menu === "create-loan"}
-						onClick={() => {
-							onClose && onClose();
-							navigate("/create-loan");
-						}}
-					>
+					<ShButton StartIcon={ShowChartIcon} to="/create-loan" onClick={onClose}>
 						Create Loan
 					</ShButton>
 				</li>
 				<li>
-					<ShButton
-						StartIcon={TuneIcon}
-						active={menu === "settings"}
-						onClick={() => {
-							onClose && onClose();
-							navigate("/settings");
-						}}
-					>
+					<ShButton StartIcon={TuneIcon} to={"/settings"} onClick={onClose}>
 						Settings
 					</ShButton>
 				</li>
--- apps/web/src/pages/Home/components/ui/ShButton.tsx
+++ apps/web/src/pages/Home/components/ui/ShButton.tsx
@@ -1,23 +1,28 @@
 import { SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 interface ShButtonProps {
 	children: React.ReactNode;
 	StartIcon: OverridableComponent<SvgIconTypeMap<object, "svg">>;
-	active?: boolean;
-	onClick?: React.MouseEventHandler<HTMLButtonElement>;
+	to: string;
+	onClick?: () => void;
 }
 
-const ShButton: React.FC<ShButtonProps> = ({ children, StartIcon, active, onClick }) => {
+const ShButton: React.FC<ShButtonProps> = ({ children, StartIcon, to, onClick }) => {
 	return (
-		<button onClick={onClick} className={`h-10 w-full text-left flex items-center hover:bg-gray-100 relative ${active ? "bg-gray-200" : ""}`}>
-			{active ? <div className="bg-black w-1 h-full absolute" /> : null}
-			<div className="mx-2 ml-3">
-				<StartIcon />
-			</div>
-			{children}
-		</button>
+		<NavLink to={to} onClick={onClick}>
+			{({ isActive }) => (
+				<div className={`h-10 w-full text-left flex items-center hover:bg-gray-100 relative ${isActive ? "bg-gray-200" : ""}`}>
+					{isActive ? <div className="bg-black w-1 h-full absolute" /> : null}
+					<div className="mx-2 ml-3">
+						<StartIcon />
+					</div>
+					{children}
+				</div>
+			)}
+		</NavLink>
 	);
 };
 
